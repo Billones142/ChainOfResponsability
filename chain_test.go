089427bd022eff1b7b74150b6fc015fec9b57a44
Billones142 <stefanomerinoderui@gmail.com>
2024-04-01 18:22:35 -0300

    nombres de test corregidos, test funcionando

--- chain_test.go
+++ chain_test.go
@@ -6,30 +6,38 @@ import (
 	main "chainOfResponsability.com/example"
 )
 
-func LogicaAutorizador_test(t *testing.T) {
+func Test_LogicaAutorizador(t *testing.T) {
 	prueba := main.Autorizador{&main.ManejadorBase{}}
 	datos := &main.PedidoDeLogin{}
 
-	valoresAProbar := []int{2004, 2005, 2006, 2007, 2008, 2009}
-	valoresQueDanError := []int{}
+	aniosMayorDeEdad := []int{2004, 2005, 2006}
+	aniosMenorDeEdad := []int{2007, 2008, 2009}
+	valoresQueDieronError := []int{}
 
-	for _, num := range valoresAProbar {
+	for _, num := range aniosMayorDeEdad {
 		datos.AnioDeNacimiento = num
-		if !prueba.Autorizar(datos) {
-			valoresQueDanError = append(valoresQueDanError, num)
+		if !prueba.Autorizar(datos) { // si no sale como se espera agregar a la lista
+			valoresQueDieronError = append(valoresQueDieronError, num)
 		}
 	}
 
-	if len(valoresQueDanError) > 0 {
-		t.Errorf("El test dio error con el/los resultados %v", valoresQueDanError)
+	for _, num := range aniosMenorDeEdad {
+		datos.AnioDeNacimiento = num
+		if prueba.Autorizar(datos) { // si no sale como se espera agregar a la lista
+			valoresQueDieronError = append(valoresQueDieronError, num)
+		}
+	}
+
+	if len(valoresQueDieronError) > 0 {
+		t.Errorf("El test dio error con el/los resultados %v", valoresQueDieronError)
 	}
 }
 
-func LogicaAutenticador_test(t *testing.T) {
+func Test_LogicaAutenticador(t *testing.T) {
 	prueba := main.Autenticador{&main.ManejadorBase{}}
 	datos := &main.PedidoDeLogin{}
 
-	if prueba.ManejarPedido(datos) {
+	if !prueba.ManejarPedido(datos) {
 		t.Errorf("No hay logica de autenticacion actualmente, deberia dar verdadero todo el tiempo")
 	}
 }
